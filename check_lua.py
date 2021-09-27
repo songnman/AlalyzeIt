5f9bd2500dec19b37001540eafc94f7b8bfa3137
Jerry <songnman@gmail.com>
2021-09-27 19:28:43 +0900

    추출 속도 개선

--- check_lua.py
+++ check_lua.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os.path
 
 from os import listdir
-from os.path import isfile, join, splitext
+from os.path import isfile, join, splitext, exists
 
 def LoadData(path):
 	code = open(path,'r', encoding='utf-8-sig')
@@ -15,25 +15,46 @@ def LoadData(path):
 	code.close()
 	data = luadata.unserialize(lines,encoding='utf-8-sig')
 	return data
+
 def ExtractUnitScript(path,DataName):
 	try:
+		if exists("temp.csv"):
+			os.remove("temp.csv")
 		my_file = f'UnitInfo/{DataName}Script.csv'
 		
-		if not os.path.exists(my_file):
+		if not exists(my_file): #TODO 파일 없을 때 새로 생성하는 부분 개선해야함.
 			f = open(my_file,'w', newline='')
 			writer = csv.writer(f)
 			writer.writerow(["UnitName","Tag","DictName","StateIndex","EventName","EventIndex","Key","Value"])
 			f.close()
-
-		unit_name = splitext(os.path.basename(path))[0]
-		data = LoadData(path)
+		
 		f = open("temp.csv",'w', newline='')
 		writer = csv.writer(f)
 		writer.writerow(["UnitName","Tag","DictName","StateIndex","EventName","EventIndex","Key","Value"])
-		StateSet_List = ["m_listAttackStateData","m_listSkillStateData","m_listHyperSkillStateData","m_listHitCriticalFeedBack"]
-		StateInfo_List = ["m_StateName"]
-		StateInfo_Avoid_List = ["m_NKM_UNIT_STATE_TYPE"]
-		state_index = 0
+		
+		for p in path:
+			for item in Writing(p):
+				writer.writerow(item)
+		f.close()
+		
+		all_filenames = ["temp.csv", my_file]
+		combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+		combined_csv = combined_csv.drop_duplicates(["UnitName","Tag","DictName","StateIndex","EventName","EventIndex","Key","Value"])
+		combined_csv.to_csv( my_file, index=False, encoding='utf-8-sig')
+		os.remove("temp.csv")
+		
+	except Exception as e:
+		print(str(e))
+def Writing(path):
+	content = []
+	StateSet_List = ["m_listAttackStateData","m_listSkillStateData","m_listHyperSkillStateData","m_listHitCriticalFeedBack"]
+	StateInfo_List = ["m_StateName"]
+	StateInfo_Avoid_List = ["m_NKM_UNIT_STATE_TYPE"]
+	state_index = 0
+		
+	unit_name = splitext(os.path.basename(path))[0]
+	data = LoadData(path)
+	if data:
 		for k, v in data.items():
 			if isinstance(v, list):
 				for item in v: #?리스트니까 For문 돌려야됨
@@ -44,9 +65,9 @@ def ExtractUnitScript(path,DataName):
 								for item in j:
 									if isinstance(item, dict):
 										for x, y in item.items():
-											writer.writerow([unit_name,tag,k,state_index,i,j.index(item)+1,x,y])
+											content.append([unit_name,tag,k,state_index,i,j.index(item)+1,x,y])
 									else:
-										writer.writerow([unit_name,tag,k,state_index,i,j.index(item)+1,item,None])
+										content.append([unit_name,tag,k,state_index,i,j.index(item)+1,item,None])
 								continue
 							elif k in StateSet_List: #?스테이트묶음 종류에 포함될 경우 스테이트묶음
 								tag = "StateSet"
@@ -54,19 +75,11 @@ def ExtractUnitScript(path,DataName):
 								tag = "StateInfo"
 							if i in StateInfo_List and k not in StateSet_List : state_index += 1 #! 인포셋에 걸리면 스테이트인덱스 증가
 							if i in StateInfo_Avoid_List and k not in StateSet_List : continue
-							writer.writerow([unit_name,tag,k,state_index,None,0,i,j])
-							# df3.update({k:v}) #! 여기부터 다시 생각해보자
+							content.append([unit_name,tag,k,state_index,None,0,i,j])
 			else:
 				tag = "BasicInfo"
-				writer.writerow([unit_name,tag,None,state_index,None,0,k,v]) #?유닛 정보
-		f.close()
-		all_filenames = ["temp.csv", my_file]
-		combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-		combined_csv = combined_csv.drop_duplicates(["UnitName","Tag","DictName","StateIndex","EventName","EventIndex","Key","Value"])
-		combined_csv.to_csv( my_file, index=False, encoding='utf-8-sig')
-		os.remove("temp.csv")
-	except Exception as e:
-		print(path +" : "+ str(e))
+				content.append([unit_name,tag,None,state_index,None,0,k,v])
+	return content
 def ExtractDamageTemplet(path):
 	try:
 		data = LoadData(path)
@@ -143,24 +156,21 @@ ScriptBase_Path = "C:\\DOC_leeseunghwan.dev\\CounterSide\\CODE\\CSClient\\Assets
 UnitScript_Path = [join(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\", f) for f in listdir(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\") if isfile(join(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\", f) ) and "NKM_UNIT_" in f and splitext(f)[1] == ".txt"]
 ShipScript_Path = [join(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\", f) for f in listdir(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\") if isfile(join(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\", f) ) and "NKM_SHIP_" in f and splitext(f)[1] == ".txt"]
 MobScript_Path = [join(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\", f) for f in listdir(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\") if isfile(join(f"{ScriptBase_Path}AB_SCRIPT_UNIT_DATA\\AB_SCRIPT_UNIT_DATA_UNIT_TEMPLET\\", f) ) and ("NKM_MONSTER_" in f or "NKM_MOB_" in f or "NKM_SHADOW_" in f) and splitext(f)[1] == ".txt"]
-DamageTemplet_Path = [join(ScriptBase_Path, f) for f in listdir(ScriptBase_Path) if isfile(join(ScriptBase_Path, f) ) and "LUA_DAMAGE_TEMPLET" in f and splitext(f)[1] == ".txt"]
+DamageTemplet_Path = [join(ScriptBase_Path, f) for f in listdir(ScriptBase_Path) if isfile(join(ScriptBase_Path, f) ) and "LUA_DAMAGE_TEMPLET" in f and "_BASE" not in f and splitext(f)[1] == ".txt"]
 DamageTemplet_Path.reverse()
 DamageEffectTemplet_Path = [join(f"{ScriptBase_Path}AB_SCRIPT_EFFECT\\", f) for f in listdir(f"{ScriptBase_Path}AB_SCRIPT_EFFECT\\") if isfile(join(f"{ScriptBase_Path}AB_SCRIPT_EFFECT\\", f) ) and "LUA_DAMAGE_EFFECT_TEMPLET" in f and splitext(f)[1] == ".txt"]
 DamageEffectTemplet_Path.reverse()
 
 print("Extract Count :", len(UnitScript_Path))
-for path in UnitScript_Path:
-	ExtractUnitScript(path,"Unit")
+ExtractUnitScript(UnitScript_Path,"Unit")
 print("UnitScript ExtractDone.")
 
 print("Extract Count :", len(ShipScript_Path))
-for path in ShipScript_Path:
-	ExtractUnitScript(path,"Ship")
+ExtractUnitScript(ShipScript_Path,"Ship")
 print("ShipScript ExtractDone.")
 
 print("Extract Count :", len(MobScript_Path))
-for path in MobScript_Path:
-	ExtractUnitScript(path,"Mob")
+ExtractUnitScript(MobScript_Path,"Mob")
 print("MobScript ExtractDone.")
 
 print("Extract Count :", len(DamageEffectTemplet_Path))
